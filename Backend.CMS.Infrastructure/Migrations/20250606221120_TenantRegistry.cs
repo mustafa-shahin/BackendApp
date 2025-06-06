37a099b63d0af1ded55798d29b8add19f9e9669c
Mustafa Shahin <109212127+mustafa-shahin@users.noreply.github.com>
2025-06-07 00:33:24 +0200

    update

---
+++ Backend.CMS.Infrastructure/Migrations/20250606221120_TenantRegistry.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.CMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TenantRegistry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
